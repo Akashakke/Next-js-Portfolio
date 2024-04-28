b67e2dd95768af1a90cdde269a2ab62f734f1439
Akash praveen kumar R <rakashpraveen@karunya.edu.in>
2024-04-28 11:56:48 +0530

    new

---
+++ app/components/Herosection.jsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+// import profile from '/images/profile-img.png'
+
+export default function Herosection() {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello I'm
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Akash",
+                1000,
+                "Web developer",
+                1000,
+                "Frontend developer",
+                1000,
+                "Full stack developer",
+                1000,
+              ]} 
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-white text-base  sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod,
+            eveniet,
+          </p>{" "}
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white ">
+              Hire Me
+            </button>
+            <button
+              className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white 
+            mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              width={300}
+              height={300}
+              className="absolute transform  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 profile-img"
+              alt="profile"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ app/components/MenuOverlay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navlink from "./Navlink";
+
+export default function MenuOverlay({ links }) {
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, i) => (
+        <li key={i}>
+          <Navlink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+}
